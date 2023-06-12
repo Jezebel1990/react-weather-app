6581c2da6cb82ff0cae182ee20d6dfa1c6515285
Jezebel1990 <jezebelguedes@gmail.com>
2023-06-12 15:58:12 -0300

    update colors

--- src/App.js
+++ src/App.js
@@ -22,9 +22,13 @@ function App() {
 
   return (
     <div>
-      <section class="relative bg-gray-900  min-h-screen">
+      <section class="relative bg-cover bg-center bg-image-url min-h-screen"
+       style={{
+        backgroundImage: 'url("https://i.imgur.com/isHNQlY.jpg")',
+       }}
+      >
         <img
-          class="w-56 lg:block lg:absolute top-0 left-0 pt-10"
+          class="w-100 lg:block lg:absolute top-0 left-0 pt-10"
           src={weatherPNG}
           alt="/"
         />
@@ -33,18 +37,18 @@ function App() {
           <h2 class="mt-8 mb-8 lg:mb-12 text-white text-4xl lg:text-6xl font-semibold">
             Tempo agora
           </h2>
-          <p class="max-w-3xl mx-auto mb-8 lg:mb-12 text-white text-xl opacity-50">
+          <p class="max-w-3xl mx-auto mb-8 lg:mb-12 text-white text-xl opacity-80">
             Descubra a temperatura atual  em todo o mundo
           </p>
           <input value={city}
             onChange={e => setCity(e.target.value)}
             placeholder="Search City"
-            class="relative z-10 inline-block w-full md:w-auto mb-2  px-3 py-2 mr-4  font-medium leading-normal bg-transparent border-2 rounded-lg text-green-400 "
+            class="relative z-10 inline-block w-full md:w-auto mb-2  px-3 py-2 mr-4  font-medium leading-normal bg-transparent border-2 rounded-lg text-yellow-400 "
           ></input>
           <button
             onClick={() => dispatch(fetchWeatherAction(city))}
             type="button"
-            className="inline-flex items-center px-3 pr-3 28 text-center py-3 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="inline-flex items-center px-3 pr-3 28 text-center py-3 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
           >
             Buscar
           </button>
@@ -59,7 +63,7 @@ function App() {
           <div class="max-w-6xl px-4 mx-auto ">
             <div class="flex flex-wrap -mx-4 justify-center">
               <div class="w-full md:w-1/3 px-4">
-                <div class="p-8 border border-blue-800 rounded-lg">
+                <div class="bg-black bg-opacity-80 rounded-lg p-4">
                   <div class="flex justify-start  items-center">
                     <span class="flex items-center justify-center w-16 h-16 rounded-full border-2">
                       <img
@@ -68,7 +72,7 @@ function App() {
                         alt="/"
                       />
                     </span>
-                    <h1 class="text-gray-300 pl-5 uppercase-text">
+                    <h1 class="text-gray-300 pl-5">
                       {weather?.weather[0].description}
                     </h1>{" "}
                   </div>
@@ -106,10 +110,10 @@ function App() {
       </section>
 
       {/* Footer */}
-      <div class="text-center">
-        <p class="mb-4  text-blue-300">
+      <div class="bg-yellow-600 bg-opacity-90 py-4 text-center">
+        <p class="mb-4  text-yellow-200">
           Desenvolvido por 
-          <span class="p-2 text-blue-300">
+          <span class="p-2 text-yellow-200">
             <a href="https://www.linkedin.com/in/jezebel-guedes/">
               Jezebel Guedes
             </a>
